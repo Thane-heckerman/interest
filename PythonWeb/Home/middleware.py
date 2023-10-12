9b6a65d3662fa84270eb5c1e6ab342e389e13203
Thane <trinhngocthanh2204@gmail.com>
2023-10-12 10:32:43 +0700

    small fix middleware function

--- PythonWeb/Home/middleware.py
+++ PythonWeb/Home/middleware.py
@@ -18,28 +18,25 @@ class SearchHistory:
             if statusTracking.objects.get(user_id = user) is not None:
                 status = statusTracking.objects.get(user_id = user)    
                 status1 = status.is_enabled
-                ignore_urls = ['/Home/interest/', '/Home/logout/', '/Home/login/', '/Home/', '/Home/history/','/Home/redirectToggle/','/Home/changeStatus/','']
+                urls = ['/Home/lowest_below_1_month/', '/Home/lowest_1_month/', '/Home/lowest_2_month/', '/Home/lowest_3_month', '/Home/lowest_4_month/','/Home/lowest_5_month/','/Home/lowest_6_month/','/Home/lowest_7_month/',
+                        '/Home/lowest_8_month/','/Home/lowest_9_month/','/Home/lowest_10_month/','/Home/lowest_11_month/','/Home/lowest_12_month/','/Home/lowest_24_month/','/Home/lowest_36_month/']
                 if status1 == True:
                     print('đang bật chức năng lưu')
                     # request.path in ignore_urls and
 
-                    if request.path in ignore_urls:
+                    if request.path not in urls:
                         # print('không được ghi lịch sử vì url trong ignore')
                         return self.get_response(request)
                     
-                    else:
-                        if  not request.path.startswith('/Home/delete/') : 
-                            # print ('k ghi chức năng delete')
-                            response = self.get_response(request)
-                            period = request.resolver_match.url_name
-                            res = str(response.content).replace('b','',1)
-                            History.objects.create(user=request.user, url=request.path, result = res, period=period)  
-                            # print('đã ghi lịch sử')
-                            return response
-                        
-                        else:
-                            # print('url có trong ignore_url nên không lưu')
-                            return self.get_response(request)   
+                    else:  
+                        # print ('k ghi chức năng delete')
+                        response = self.get_response(request)
+                        period = request.resolver_match.url_name
+                        res = str(response.content).replace('b','',1)
+                        History.objects.create(user=request.user, url=request.path, result = res, period=period)  
+                        # print('đã ghi lịch sử')
+                        return response
+                           
                 else:
                     response = self.get_response(request)
                     return response
